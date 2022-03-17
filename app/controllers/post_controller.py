b822323c06b733c8ac3023fd570c9b2862ea0326
Bernardo <bernardocardosodacosta@gmail.com>
2022-03-17 16:30:27 -0300

    feat: connect to a remote mongodb database

--- app/controllers/post_controller.py
+++ app/controllers/post_controller.py
@@ -1,15 +1,17 @@
-from http import client
 from flask import request, jsonify
 import pymongo 
+from pymongo import MongoClient
 
-client = pymongo.MongoClient("mongodb://localhost:27017/")
+cluster = MongoClient(
+    "mongodb+srv://ninth_avaliation:XQ4hi36Wi5mgxGk1@cluster0.nmb8m.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+)
 
-db = client["kenzie"]
+db = cluster["kenzie"]
 
 collection = db["posts"]
 
 def post():
     data = request.get_json()
-    db.crud.insert_one(data)
+    db.posts.insert_one(data)
     del data["_id"]
     return jsonify(data), 201
